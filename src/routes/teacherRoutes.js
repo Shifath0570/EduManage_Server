dd0ef4185de9fcedf68f855baa3d7430e84e7486
actuallyayon <actuallyayon@gmail.com>
2026-09-05 00:49:22 +0600

    fix(routes): fix teacher handler binding to getAllTeachers

--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/auth');
 
-const { getTeachers, getTeacherById, createTeacher, updateTeacher, deleteTeacher} = require('../controllers/teacherController');
+const { getAllTeachers, getTeacherById, createTeacher, updateTeacher, deleteTeacher } = require('../controllers/teacherController');
 
 // Public routes
-router.get('/', getTeachers);
+router.get('/', getAllTeachers);
 router.get('/:id', getTeacherById);
 
 // Protected routes (require authentication)
