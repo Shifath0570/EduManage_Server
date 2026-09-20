99e3671ac682138521cffdcdc2eebd220c03e40a
Osman Goni <osman.mirpur55@gmail.com>
2026-09-20 19:33:57 +0600

    change notice and sturent routes

--- src/routes/noticeRoutes.js
+++ src/routes/noticeRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const { verifyToken, authorize } = require('../middleware/auth');
 const { getNotices, getNoticeById, createNotice, deleteNotice, updateNotice } = require('../controllers/noticeController');
 
 
 
-// Public route to get all notices
 router.get('/', getNotices);
 router.get('/:id', getNoticeById);
-router.delete('/:id', deleteNotice);
-router.post('/', createNotice); 
-router.put('/:id', updateNotice); 
+router.delete('/:id', verifyToken, authorize(['admin']), deleteNotice);
+router.post('/', verifyToken, authorize(['admin']), createNotice);
+router.put('/:id', verifyToken, authorize(['admin']), updateNotice); 
 
 
 
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const { verifyToken, authorize } = require('../middleware/auth');
 const { getStudents, getStudentById, createStudent, updateStudent, deleteStudent} = require('../controllers/studentController');
 
 // Public routes
-router.get('/', getStudents);
-router.get('/:id', getStudentById);
+router.get('/', verifyToken, authorize('admin', 'teacher', 'student'), getStudents);
+router.get('/:id', verifyToken, authorize('admin', 'teacher', 'student'), getStudentById);
 
 // Protected routes (require authentication)
-router.post('/', createStudent);
-router.put('/:id', updateStudent);
-router.delete('/:id', deleteStudent);
+router.post('/', verifyToken, authorize('admin', 'teacher'), createStudent);
+router.put('/:id', verifyToken, authorize('admin', 'teacher'), updateStudent);
+router.delete('/:id', verifyToken, authorize('admin'), deleteStudent);
 
 module.exports = router;
 
